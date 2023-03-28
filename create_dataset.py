a83460716fbc717d94404d0a6ea929aba396e73b
Kelly Guo <kellyg@nvidia.com>
2023-03-27 22:22:08 -0400

    mobilenet_test87

--- create_dataset.py
+++ create_dataset.py
@@ -32,7 +32,7 @@ def augment_dataset(img, label, count, file, seed=42):
         # new_img = tf.image.stateless_random_contrast(image=new_img, lower=0.5, upper=1.5, seed=(seed, seed))
         # new_img = tf.image.stateless_random_flip_left_right(image=new_img, seed=(seed, seed))
         # new_img = tf.image.stateless_random_flip_up_down(image=new_img, seed=(seed, seed))
-        new_img = tf.image.stateless_random_hue(image=img, max_delta=0.2, seed=(seed, seed))
+        new_img = tf.image.stateless_random_hue(image=img, max_delta=0.05, seed=(seed, seed))
         new_img = tf.image.stateless_random_saturation(image=new_img, lower=0.75, upper=1.25, seed=(seed, seed))
 
         flip = tf.keras.layers.RandomFlip("horizontal_and_vertical")
@@ -81,9 +81,9 @@ for label in label_names:
 print(labels_map)
 print(num_items)
 
-total_wanted = 5000
+total_wanted = 2500
 for d, l, f in zip(data, labels, file_paths):
-    f = ''.join(f.split("/")[-1].split(".")[:-1])
+    f = ''.join(os.path.basename(f).split(".")[:-1])
     label = labels_map[l]
     existing_count = num_items[label]
     count_per_img = total_wanted // existing_count
--- tea_leaf_vgg.py
+++ tea_leaf_vgg.py
@@ -62,11 +62,12 @@ def visualize_dataset(training_data, augmentation=None):
         plt.imshow(img)
     plt.show()
 
-directory = 'tea_dataset_merged'
-batch_size = 32
+# directory = 'tea_dataset_merged'
+directory = '../tea_leaf_augmented'
+batch_size = 256
 image_size = (256, 256)
 seed = 42
-epochs = 20
+epochs = 10
 
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     directory,
@@ -101,14 +102,14 @@ val_batches = tf.data.experimental.cardinality(val_dataset)
 test_dataset = val_dataset.take((2*val_batches) // 3)
 val_dataset = val_dataset.skip((2*val_batches) // 3)
 
-augmentation = augmentation_layers()
+# augmentation = augmentation_layers()
 # visualize_dataset(train_dataset, augmentation)
 
 train_dataset = train_dataset.map(normalize)
 val_dataset = val_dataset.map(normalize)
 test_dataset = test_dataset.map(normalize)
 
-train_dataset = train_dataset.map(lambda x, y: (augmentation(x, training=True), y))
+# train_dataset = train_dataset.map(lambda x, y: (augmentation(x, training=True), y))
 
 def build_vgg16_raw():
     vgg = tf.keras.applications.VGG16(
@@ -145,7 +146,9 @@ def build_resnet50():
         input_shape=(256, 256, 3),
     )
     vgg.trainable = False
-    x = tf.keras.layers.Flatten()(vgg.output)
+    x = tf.keras.layers.MaxPool2D()(vgg.output)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
     x = tf.keras.layers.Dense(9, activation='softmax')(x)
     model = tf.keras.Model(inputs=vgg.input, outputs=x)
     model.summary()
@@ -158,7 +161,8 @@ def build_mobilenetv2():
         input_shape=(256, 256, 3),
     )
     vgg.trainable = False
-    x = tf.keras.layers.Flatten()(vgg.output)
+    x = tf.keras.layers.MaxPool2D()(vgg.output)
+    x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(9, activation='softmax')(x)
     model = tf.keras.Model(inputs=vgg.input, outputs=x)
     model.summary()
@@ -189,13 +193,13 @@ def test(model, test_dataset):
     test_loss, test_acc = model.evaluate(test_dataset)
     print(test_loss, test_acc)
 
-model = build_vgg16_raw()
+# model = build_vgg16_raw()
 # model = build_vgg19_raw()
-# model = build_mobilenetv2()
+model = build_mobilenetv2()
 # model = build_resnet50()
 # model = build_inception_resnetv2()
 
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
 history = train(model, train_dataset, val_dataset)
 test(model, test_dataset)
 
